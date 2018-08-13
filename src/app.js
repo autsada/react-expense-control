ef591d07221db2d4e3d9bcfca9a59eaaa25ec0ed
Autsada <autsada.t@gmail.com>
2018-08-13 08:47:41 +0700

    Remove dummy expenses data

--- src/app.js
+++ src/app.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense, removeExpense, editExpense } from './actions/expenses';
-import { setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate } from './actions/filters';
-import getVisibleExpenses from './selectors/getVisibleExpenses';
+// import { addExpense, removeExpense, editExpense } from './actions/expenses';
+// import { setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate } from './actions/filters';
+//import getVisibleExpenses from './selectors/getVisibleExpenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
@@ -16,24 +16,23 @@ const store = configureStore();
 //store.dispatch(addExpense({ description: 'Water bill', amount: 100, createdAt: 1000 }));
 //store.dispatch(addExpense({ description: 'Gas bill', amount: 300, createdAt: -1000 }));
 //store.dispatch(addExpense({ description: 'Rent', amount: 10300, createdAt: 500 }));
-console.log(store.getState());
+// console.log(store.getState());
 
-// store.dispatch(setTextFilter('water'));
+// // store.dispatch(setTextFilter('water'));
 
-// setTimeout(() => {
-//     store.dispatch(setTextFilter('bill'));
-// }, 3000); 
+// // setTimeout(() => {
+// //     store.dispatch(setTextFilter('bill'));
+// // }, 3000); 
 
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+// const state = store.getState();
+// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 
-console.log(visibleExpenses);
+// console.log(visibleExpenses);
 
 const jsx = (
     <Provider store={store}>
         < AppRouter />
     </Provider>
-    
 );
 
 ReactDOM.render(jsx, document.getElementById('app'));
