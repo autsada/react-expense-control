708855618c2c47c9b5c08830cc8acad1db2d1f5f
Autsada <autsada.t@gmail.com>
2018-08-20 14:31:24 +0700

    Add PrivateRoute

--- src/app.js
+++ src/app.js
@@ -1,44 +1,72 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import AppRouter from './routers/AppRouter';
+import AppRouter,  { history } from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { startSetExpenses } from './actions/expenses';
-// import { setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate } from './actions/filters';
-//import getVisibleExpenses from './selectors/getVisibleExpenses';
+import { login, logout } from './actions/auth';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
-import './firebase/firebase';
+import{ firebase } from './firebase/firebase';
 //import './playground/promises';
 
 const store = configureStore();
-//console.log(store.getState());
 
-//store.dispatch(addExpense({ description: 'Water bill', amount: 100, createdAt: 1000 }));
-//store.dispatch(addExpense({ description: 'Gas bill', amount: 300, createdAt: -1000 }));
-//store.dispatch(addExpense({ description: 'Rent', amount: 10300, createdAt: 500 }));
-// console.log(store.getState());
-
-// // store.dispatch(setTextFilter('water'));
-
-// // setTimeout(() => {
-// //     store.dispatch(setTextFilter('bill'));
-// // }, 3000); 
-
-// const state = store.getState();
-// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-
-// console.log(visibleExpenses);
-//console.log('test')
 const jsx = (
     <Provider store={store}>
         < AppRouter />
     </Provider>
 );
 
+let hasRendered = false;
+const renderApp = () => {
+    if (!hasRendered) {
+        ReactDOM.render(jsx, document.getElementById('app'));
+        hasRendered = true;
+    };
+};
+
 ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
 
-store.dispatch(startSetExpenses()).then(() => {
-    ReactDOM.render(jsx, document.getElementById('app'));
+// const authentication = () => {
+//     return (dispatch) => {
+//         return firebase.auth().onAuthStateChanged((user)).then(() => {
+//             if (user) {
+//                 dispatch(login(user.uid));
+//                 dispatch(startSetExpenses()).then(() => {
+//                     renderApp();
+//                     if (history.location.pathname === '/') {
+//                         history.push('/dashboard');
+//                     }    
+//                 });        
+//                 console.log('log in');
+//             } else {
+//                 dispatch(logout());
+//                 renderApp();
+//                 history.push('/');
+//                 console.log('logout');
+//             }
+//         });
+//     }
+// };
+// authentication();
+
+firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+        store.dispatch(login(user.uid));
+        store.dispatch(startSetExpenses()).then(() => {
+            renderApp();
+
+            if (history.location.pathname === '/') {
+                history.push('/dashboard');
+            }
+        });
+        console.log('log in');
+    } else {
+        store.dispatch(logout());
+        renderApp();
+        history.push('/');
+        console.log('logout');
+    }
 });
